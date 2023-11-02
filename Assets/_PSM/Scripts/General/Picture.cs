2051cfdcd4f0da105c9302da21093a034b44e44f
MaxBrons <43110145+MaxBrons@users.noreply.github.com>
2023-11-02 14:10:39 +0100

    Implemented new Art

--- Assets/_PSM/Scripts/General/Picture.cs
+++ Assets/_PSM/Scripts/General/Picture.cs
@@ -1,4 +1,5 @@
 using System;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,12 +8,16 @@ public class Picture : MonoBehaviour
     public event Action<Picture> OnPictureSelected;
 
     public Sprite Sprite => _picture;
+    public string Name => _name;
 
     [SerializeField] private Sprite _picture;
     [SerializeField] private Image _pictureUI;
+    [SerializeField] private TMP_Text _nameText;
+    [SerializeField] private string _name;
 
     private void Start()
     {
+        _nameText.text = _name;
         _pictureUI.sprite = Sprite;
     }
 
